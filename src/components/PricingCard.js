7e261d707dee94f47e398070a4510f3755338e20
mohammad usman mir <mirusman21421@gmail.com>
2023-02-04 22:32:58 +0530

    adding few changes

---
+++ src/components/PricingCard.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import "./PricingCardStyles.css"
+
+import React from 'react'
+
+const PricingCard = () => {
+  return ( 
+    <div className="pricing">
+    <div className="card-container">
+        <div className="card">
+            <h3>-Basic-</h3>
+            <span className="bar"></span>
+            <p className="btc"> $100</p>
+            <p> -3 Days- </p>
+            <p>-3 Pages-</p>
+            <p>-Responsive Design</p>
+            <Link to="/Contact" className="btn">
+                PURCHASE NOW
+            </Link>
+
+        </div>
+
+        <div className="card">
+            <h3>-Premium-</h3>
+            <span className="bar"></span>
+            <p className="btc"> $200</p>
+            <p> -4 Days- </p>
+            <p>-6 Pages-</p>
+            <p>-Responsive Design</p>
+            <Link to="/Contact" className="btn">
+                PURCHASE NOW
+            </Link>
+
+        </div>
+
+
+        <div className="card">
+            <h3>-Business-</h3>
+            <span className="bar"></span>
+            <p className="btc"> $100</p>
+            <p> -6 Days- </p>
+            <p>-10 Pages-</p>
+            <p>-Responsive Design</p>
+            <Link to="/Contact" className="btn">
+                PURCHASE NOW
+            </Link>
+
+        </div>
+    </div>
+    </div>
+  );
+};
+
+export default PricingCard;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import PricingCard from '../components/PricingCard';
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2  heading="Projects" text="My Recent Projects"/>
+      <PricingCard />
       <Footer />
     </div>
   )
